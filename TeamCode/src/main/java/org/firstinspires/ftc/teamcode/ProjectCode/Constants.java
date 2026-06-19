a64f6d7c163af9946ddb0d53744c1cf485213d06
akash <Akashvijayaradhya@gmail.com>
2026-06-18 19:06:06 -0700

    Pedo Tuning Push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProjectCode/Constants.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.ProjectCode;
+
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.control.PredictiveBrakingCoefficients;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(12.29235)
+            .forwardZeroPowerAcceleration(-47.11204666214807)
+            .lateralZeroPowerAcceleration(-55.07048078651405)
+
+            .predictiveBrakingCoefficients(new PredictiveBrakingCoefficients(0.1, 0.043469896847401546, 0.002773203922513006))
+
+            .useSecondaryTranslationalPIDF(false)
+            .useSecondaryHeadingPIDF(false)
+            .useSecondaryDrivePIDF(false)
+            .centripetalScaling(0.0003)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.08, 0, 0.000001, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.07, 0))
+            .drivePIDFCoefficients(
+                    new FilteredPIDFCoefficients(0.014, 0, 0.000005, 0.085, 0.003)
+            )
+            .secondaryTranslationalPIDFCoefficients(
+                    new PIDFCoefficients(0.11, 0, 0, 0)
+            );
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .leftFrontMotorName("lfmotor")
+            .leftRearMotorName("lbmotor")
+            .rightFrontMotorName("rfmotor")
+            .rightRearMotorName("rbmotor")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(79.53968582754061)
+            .yVelocity(57.234300027682096);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-3.305)
+            .strafePodX(-5.15)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .yawScalar(1.000008396143041)
+            .encoderResolution(
+                    GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD
+            )
+            //.customEncoderResolution(13.26291192)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            500,
+            0.800,
+            1
+    );
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .build();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProjectCode/auto.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.ProjectCode;
+
+public class auto {
+}
